1f2c2ed907d3ee696eb28bdca620dcb3d884f46c
tanny nogales <tanny.nogales@gmail.com>
2023-08-21 11:08:20 -0400

    parent category update: add product list

--- src/app/features/shop-parent-category/pages/shop-parent-category-home/shop-parent-category-home.component.ts
+++ src/app/features/shop-parent-category/pages/shop-parent-category-home/shop-parent-category-home.component.ts
@@ -39,4 +39,5 @@ export class ShopParentCategoryHomeComponent {
       slug: 'Otros',
     },
   ];
+  products = [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}];
 }
--- src/app/features/shop-parent-category/shop-parent-category-routing.module.ts
+++ src/app/features/shop-parent-category/shop-parent-category-routing.module.ts
@@ -1,7 +1,5 @@
-import { CarouselComponent } from './../../shared/components/carousel/carousel.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ShopCartHomeComponent } from '@features/shop-cart/pages/shop-cart-home/shop-cart-home.component';
 import { ShopParentCategoryHomeComponent } from './pages/shop-parent-category-home/shop-parent-category-home.component';
 
 const routes: Routes = [
