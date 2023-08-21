da9bc8b86b5363f468c419f09f09aadb30625d74
tanny nogales <tanny.nogales@gmail.com>
2023-08-21 13:54:01 -0400

    test share

--- src/app/features/shop-cart/pages/shop-cart-home/shop-cart-home.component.ts
+++ src/app/features/shop-cart/pages/shop-cart-home/shop-cart-home.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-shop-cart-home',
   templateUrl: './shop-cart-home.component.html',
-  styleUrls: ['./shop-cart-home.component.scss']
+  styleUrls: ['./shop-cart-home.component.scss'],
 })
 export class ShopCartHomeComponent {
-
+  test() {
+    navigator.share({
+      url: 'https://google.com',
+      title: 'Test title',
+      text: 'Test text',
+    });
+  }
 }
