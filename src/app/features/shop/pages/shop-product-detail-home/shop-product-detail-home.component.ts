3d2512948640c3c2b548ca39c36654329083cf59
Tanny Nogales <tanny.nogales@gmail.com>
2023-08-23 13:20:05 -0400

    add share feature in prduct detail

--- src/app/features/shop/pages/shop-product-detail-home/shop-product-detail-home.component.ts
+++ src/app/features/shop/pages/shop-product-detail-home/shop-product-detail-home.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shop-product-detail-home',
   templateUrl: './shop-product-detail-home.component.html',
-  styleUrls: ['./shop-product-detail-home.component.scss']
+  styleUrls: ['./shop-product-detail-home.component.scss'],
 })
 export class ShopProductDetailHomeComponent {
+  public href: string = '';
+  isShareEnable = false;
 
+  constructor(private router: Router) {}
+  ngOnInit() {
+    this.href = this.router.url;
+    this.isShareEnable = 'share' in navigator;
+  }
+
+  share() {
+    navigator
+      .share({
+        title: 'Título del contenido compartido',
+        text: 'Texto del contenido compartido',
+        url: this.href,
+      })
+      // .then(() => {})
+      .catch((error) => console.error('Error al compartir:', error));
+  }
 }
