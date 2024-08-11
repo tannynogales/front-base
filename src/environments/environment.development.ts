11ee752589f6cc346164ebf683b5c09a31ddf0ba
Tanny Nogales <tanny.nogales@gmail.com>
2024-08-11 13:04:20 -0400

    Revert "change back url"
    This reverts commit 6db558e6baac653bb981e98147b1fc16980acb1c.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,8 +3,7 @@ export const environment = {
     'AS_ickX92sOz3UrhKSdFu7nHE8RfNfnP-HuZQfBwI-_Q8l119DyMtohNSpqhsQpLQdtji8gGwTs5JAQT',
   //https://developer.paypal.com/docs/reports/reference/paypal-supported-currencies/
   paypalCurrency: 'EUR', //USD
-  // strapi: 'https://roble-strapi.rj.r.appspot.com',
-  strapi: 'https://neges-strapi-production.up.railway.app',
+  strapi: 'https://roble-strapi.rj.r.appspot.com',
   // strapi: 'http://localhost:1337',
   strapiSiteID: 1,
   sessionExpirationTime: 5, // minutes
