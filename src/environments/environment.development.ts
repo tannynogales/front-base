6db558e6baac653bb981e98147b1fc16980acb1c
Tanny Nogales <tanny.nogales@gmail.com>
2024-08-11 01:54:13 -0400

    change back url

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,8 @@ export const environment = {
     'AS_ickX92sOz3UrhKSdFu7nHE8RfNfnP-HuZQfBwI-_Q8l119DyMtohNSpqhsQpLQdtji8gGwTs5JAQT',
   //https://developer.paypal.com/docs/reports/reference/paypal-supported-currencies/
   paypalCurrency: 'EUR', //USD
-  strapi: 'https://roble-strapi.rj.r.appspot.com',
+  // strapi: 'https://roble-strapi.rj.r.appspot.com',
+  strapi: 'https://neges-strapi-production.up.railway.app',
   // strapi: 'http://localhost:1337',
   strapiSiteID: 1,
   sessionExpirationTime: 5, // minutes
