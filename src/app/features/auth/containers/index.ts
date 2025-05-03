f3b752e10b70fb62a4d7355e1919b126048971bc
Tanny Nogales <tannynogales@Tanny-Nogales-MacBook-Pro.local>
2025-05-03 17:19:14 -0400

    delete email-confirmation

--- src/app/features/auth/containers/index.ts
+++ src/app/features/auth/containers/index.ts
@@ -2,7 +2,6 @@ import { LoginComponent } from './login/login.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { LogOutComponent } from './log-out/log-out.component';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
-import { EmailConfirmationComponent } from './email-confirmation/email-confirmation.component';
 import { RegisterSuccessComponent } from './register-success/register-success.component';
 import { EmailNotConfirmedComponent } from './email-not-confirmed/email-not-confirmed.component';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
@@ -12,7 +11,7 @@ export const containers = [
   SignUpComponent,
   LogOutComponent,
   ResetPasswordComponent,
-  EmailConfirmationComponent,
+
   RegisterSuccessComponent,
   EmailNotConfirmedComponent,
   ForgotPasswordComponent,
@@ -22,7 +21,6 @@ export * from './login/login.component';
 export * from './sign-up/sign-up.component';
 export * from './log-out/log-out.component';
 export * from './reset-password/reset-password.component';
-export * from './email-confirmation/email-confirmation.component';
 export * from './register-success/register-success.component';
 export * from './email-not-confirmed/email-not-confirmed.component';
 export * from './forgot-password/forgot-password.component';
